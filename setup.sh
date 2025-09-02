01f74a1b20a1875ef169dc4e88a26e0689b03117
UniversPlayer <158196978+UniversPlayer@users.noreply.github.com>
2025-09-02 23:04:32 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,23 @@
 
 echo "Made by ItsNickOp"
 
-# Ask for token and channel ID (input will be hidden)
+# Ask for token and channel ID
 read -s -p "Token: " TOKEN
 echo
 read -s -p "Channel ID: " CHANNEL_ID
 echo
 
-# Save these to environment for bot.py to use
+# Save these to environment
 export DISCORD_TOKEN=$TOKEN
 export CHANNEL_ID=$CHANNEL_ID
 
-# Install required libraries
+# Download required files
+curl -s -O https://raw.githubusercontent.com/UniversPlayer/discord_temp_mail_bot/main/bot.py
+curl -s -O https://raw.githubusercontent.com/UniversPlayer/discord_temp_mail_bot/main/mail_utils.py
+curl -s -O https://raw.githubusercontent.com/UniversPlayer/discord_temp_mail_bot/main/requirements.txt
+
+# Install dependencies
 pip install -r requirements.txt
 
-# Start the bot
+# Run the bot
 python3 bot.py
